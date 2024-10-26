ede3152cd4dce4a5de5abf5155d2ee6a8ee54832
Harut00t <seiichipaiko@iskolarngbayan.pup.edu.ph>
2024-10-27 00:49:58 +0800

    This is the pseudocode for my program, this is still vague but I have the grasp of the its conceptual flow. If this did not work, then I will change it. :)

---
+++ range_1_to_50_counter_program.py
@@ -0,0 +1,20 @@
+#Create a list for the input numbers
+
+"""If this did not work, then I will try to do it as a variable."""
+
+#Create a loop for the repetition of input numbers
+
+#Create a loop for the input numbers
+
+#list append
+
+#loop for:
+#Make an error message for invalid input and;
+    #Execute the comparatives (if and else) and add 1 for each category:
+        #1 to 10
+        #11 to 20
+        #21 to 30
+        #31 to 40
+        #41 to 50
+
+#print the range of numbers
